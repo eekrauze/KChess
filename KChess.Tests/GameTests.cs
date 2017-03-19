4523f1074a140c2f08fc2db97df2c139ee945427
Ekaterina Krauze <ekaterina.krauze@gmail.com>
2017-03-19 19:11:53 +0300

    Added new tests

--- KChess.Tests/GameTests.cs
+++ KChess.Tests/GameTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KChess.Tests
 {
@@ -14,6 +15,13 @@ namespace KChess.Tests
             Assert.AreEqual(0, Game.GetSquareColor("b7"));
             Assert.AreEqual(0, Game.GetSquareColor("h3"));
             Assert.AreEqual(0, Game.GetSquareColor("c4"));
+
+            Assert.AreEqual(1, Game.GetSquareColor("A1"));
+            Assert.AreEqual(1, Game.GetSquareColor("D4"));
+            Assert.AreEqual(1, Game.GetSquareColor("F6"));
+            Assert.AreEqual(0, Game.GetSquareColor("B7"));
+            Assert.AreEqual(0, Game.GetSquareColor("H3"));
+            Assert.AreEqual(0, Game.GetSquareColor("C4"));
         }
 
         [TestMethod]
@@ -21,6 +29,9 @@ namespace KChess.Tests
         {
             Assert.AreEqual(true, Game.IsBlack("a1"));
             Assert.AreEqual(false, Game.IsBlack("a8"));
+
+            Assert.AreEqual(false, Game.IsBlack("B1"));
+            Assert.AreEqual(true, Game.IsBlack("B8"));
         }
 
         [TestMethod]
@@ -28,6 +39,51 @@ namespace KChess.Tests
         {
             Assert.AreEqual(true, Game.IsWhite("d1"));
             Assert.AreEqual(false, Game.IsWhite("d8"));
+
+            Assert.AreEqual(true, Game.IsWhite("E2"));
+            Assert.AreEqual(false, Game.IsWhite("E7"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGetSquareColorForInvalidCoordinate1()
+        {
+            Game.GetSquareColor("x1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGetSquareColorForInvalidCoordinate2()
+        {
+            Game.GetSquareColor("a9");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGetSquareColorForInvalidCoordinate3()
+        {
+            Game.GetSquareColor("x9");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGetSquareColorForInvalidCoordinate4()
+        {
+            Game.GetSquareColor("x-1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestIsWhiterForInvalidCoordinate()
+        {
+            Game.IsWhite("x1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestIsBlackForInvalidCoordinate()
+        {
+            Game.IsBlack("a9");
         }
     }
 }
