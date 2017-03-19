2078deca52454e4c750c7d68472341ae3e86fc37
Ekaterina Krauze <ekaterina.krauze@gmail.com>
2017-03-19 15:56:50 +0300

    Added console and library projects to solution

---
+++ KChess.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KChess.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Game.Name);
+            System.Console.ReadKey();
+        }
+    }
+}
