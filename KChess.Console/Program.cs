bc4ca0f3c41b3a9ae53c1f589529a93098f707cc
Ekaterina Krauze <ekaterina.krauze@gmail.com>
2017-03-19 18:30:18 +0300

    Added test project

--- KChess.Console/Program.cs
+++ KChess.Console/Program.cs
@@ -11,10 +11,6 @@ namespace KChess.Console
         static void Main(string[] args)
         {
             System.Console.WriteLine(Game.Name);
-            System.Console.WriteLine(Game.GetSquareColor("a3"));
-            System.Console.WriteLine(Game.GetSquareColor("b5"));
-            System.Console.WriteLine(Game.GetSquareColor("f6"));
-            System.Console.WriteLine(Game.GetSquareColor("h5"));
             System.Console.ReadKey();
         }
     }
---
+++ KChess.Tests/GameTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KChess.Tests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void TestGetSquareColor()
+        {
+            Assert.AreEqual(1, Game.GetSquareColor("a1"));
+            Assert.AreEqual(1, Game.GetSquareColor("d4"));
+            Assert.AreEqual(1, Game.GetSquareColor("f6"));
+            Assert.AreEqual(0, Game.GetSquareColor("b7"));
+            Assert.AreEqual(0, Game.GetSquareColor("h3"));
+            Assert.AreEqual(0, Game.GetSquareColor("c4"));
+        }
+    }
+}
