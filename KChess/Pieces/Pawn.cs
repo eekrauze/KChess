e7844b1513bd7e94f7e81e1a424ff5a5e8ff6a83
Ekaterina Krauze <ekaterina.krauze@gmail.com>
2017-03-20 23:35:28 +0300

    Added chess pieces

---
+++ KChess/Pieces/Pawn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KChess.Pieces
+{
+    public class Pawn : Piece
+    {
+        public Pawn(Color color) : base(color)
+        {
+        }
+    }
+}
---
+++ KChess/Pieces/Piece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KChess.Pieces
+{
+    public abstract class Piece
+    {
+        public Color Color { get; }
+
+        public Piece(Color color)
+        {
+            this.Color = color;
+        }
+
+//        public abstract void Move();
+    }
+}
